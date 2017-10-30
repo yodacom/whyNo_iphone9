589ebd5f7d999b6e54a52c14eca64bbce1e982e2
Jeremy Black <jb@yodacom.com>
2017-10-30 08:27:30 -0600

    change

--- 789.js
+++ 789.js
@@ -1,9 +1,13 @@
-const 7 = "seven";
-let 8 = "eight";
-const 9 = "nine";
+const _7 = "seven";
+let _8 = "eight";
+const _9 = "nine";
 
-var whyNoVersion9 = (7,8,9) => ({
-  `Why is there no iPhone version 9? because ${7} ${8} ${9}`
+var whyNoVersion9 = (_7, _8, _9) => ({
+  Answer: `Why is there no iPhone version 9? because ${_7} ${_8} ${_9}`
 });
 
-whyNoVersion9(7,8,9);
+whyNoVersion9(_7, _8, _9);
+
+var change8 (_8) => {
+  let _8 = "ate";
+}
