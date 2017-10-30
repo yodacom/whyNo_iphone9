490dc7f5fb41ab1618b77d1d5c6d4f84ceab1ddc
Jeremy Black <jb@yodacom.com>
2017-10-30 07:57:47 -0600

    initial repository

---
+++ 789.js
@@ -0,0 +1,9 @@
+const 7 = "seven";
+let 8 = "eight";
+const 9 = "nine";
+
+var whyNoVersion9 = (7,8,9) => ({
+  `Why is there no iPhone version 9? because ${7} ${8} ${9}`
+});
+
+whyNoVersion9(7,8,9);
